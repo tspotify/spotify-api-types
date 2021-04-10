f3b7c46c0e4c298bc1bff1c0c50921ced7480a05
Shubham Parihar <shubhamparihar391@gmail.com>
2021-04-10 19:12:18 +0530

    types(rest): add typings for queries and responses related to albums

--- v1/index.ts
+++ v1/index.ts
@@ -1,1 +1,2 @@
 export * from './payloads/index';
+export * from './rest/index';
---
+++ v1/rest/album.ts
@@ -0,0 +1,63 @@
+import type { AlbumObject } from '../payloads/album';
+import type { SimplifiedTrackObject } from '../payloads/track';
+import type { PagingObject } from '../payloads/misc';
+
+/**
+ * @see https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-multiple-albums
+ */
+export interface GetMultipleAlbumObjectQuery {
+  /**
+   * A comma-separated list of the Spotify IDs for the albums. Maximum: 20 IDs
+   */
+  ids: Array<string>;
+
+  /**
+   * An `ISO 3166-1 alpha-2` country code or the string `from_token`. Provide this parameter if you want to apply `Track Relinking`
+   * 
+   * @see https://developer.spotify.com/documentation/general/guides/track-relinking-guide/
+   */
+  market?: string;
+}
+
+/**
+ * @see https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-multiple-albums
+ */
+export interface GetMultipleAlbumObjectResponse {
+  albums: Array<AlbumObject | null>;
+}
+
+/**
+ * @see https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-album
+ */
+export interface GetSingleAlbumObjectQuery extends Omit<GetMultipleAlbumObjectQuery, 'ids'> {
+  /**
+   * The Spotify ID of the album
+   */
+  id: string;
+}
+
+/**
+ * @see https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-album
+ */
+export type GetSingleAlbumObjectResponse = AlbumObject;
+
+
+/**
+ * @see https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-albums-tracks
+ */
+export interface GetSimplifiedTrackObjectPagingObjectQuery extends Omit<GetMultipleAlbumObjectQuery, 'ids'> {
+  /**
+   * The maximum number of tracks to return. Default: `20`, Minimum: `1`, Maximum: `50`
+   */
+  limit?: number;
+
+  /**
+   * The index of the first track to return. Default: 0 (the first object). Use with limit to get the next set of tracks
+   */
+  offset?: number
+}
+
+/**
+ * @see https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-albums-tracks
+ */
+export type GetSimplifiedTrackObjectPagingObjectResponse = PagingObject<SimplifiedTrackObject>;
