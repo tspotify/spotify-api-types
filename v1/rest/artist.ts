fe29ef40082e003e70fe3ae2b949cd7173002067
Shubham Parihar <shubhamparihar391@gmail.com>
2021-04-11 14:47:49 +0530

    types(rest): add typings for artist endpoints

---
+++ v1/rest/artist.ts
@@ -0,0 +1,121 @@
+import type { ArtistObject } from '../payloads/artist';
+import type { TrackObject } from '../payloads/track';
+import type { PagingObject } from '../payloads/misc';
+import type { SimplifiedAlbumObject } from '../payloads/album';
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-multiple-artists
+ */
+export interface GetMultipleArtistsQuery {
+  /**
+   * A comma-separated list of the Spotify IDs for the artists. Maximum: `50` IDs
+   */
+  ids: Array<string>;
+}
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-multiple-artists
+ */
+export interface GetMultipleArtistsResponse {
+  artists: Array<ArtistObject | null>;
+}
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-artist
+ */
+export type GetArtistQuery = null;
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-artist
+ */
+export type GetArtistResponse = ArtistObject;
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-artists-top-tracks
+ */
+export interface GetArtistTopTracksQuery {
+  /**
+   * An `ISO 3166-1 alpha-2` country code or the string `from_token`. Synonym for country
+   */
+  market: string;
+}
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-artists-top-tracks
+ */
+export interface GetArtistTopTracksResponse {
+  /**
+   * An array of up to `10` track objects in JSON format
+   */
+  tracks: Array<TrackObject>
+}
+
+/**
+ * Get Spotify catalog information about artists similar to a given artist. Similarity is based on analysis of the Spotify community’s listening history
+ * 
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-artists-related-artists
+ */
+export type GetRelatedArtistsQuery = null;
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-artists-related-artists
+ */
+export interface GetRelatedArtistsResponse {
+  /**
+   * An array of up to `20` artist objects in JSON format
+   */
+  artists: Array<ArtistObject>;
+}
+
+/**
+ * Get Spotify catalog information about an artist’s albums
+ * 
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-artists-albums
+ */
+export interface GetArtistAlbumsQuery {
+  /**
+   * A comma-separated list of keywords that will be used to filter the response. If not supplied, all album types will be returned. Valid values are:
+   * - album
+   * - single
+   * - appears_on
+   * - compilation
+   * 
+   * For example: `include_groups=album,single`
+   */
+  include_grups?: Array<string>
+
+  /**
+   * Synonym for country. `An ISO 3166-1 alpha-2` country code or the string `from_token`.
+   * Supply this parameter to limit the response to one particular geographical market.
+   * 
+   * For example, for albums available in Sweden: `market=SE`
+   * 
+   * If not given, results will be returned for all markets and you are likely to get duplicate results per album, one for each market in which the album is available!
+   */
+  market?: string;
+
+  /**
+   * The number of album objects to return.
+   * 
+   * Default: `20`, Minimum: `1`, Maximum: `50`
+   * 
+   * For example: `limit=2`
+   */
+  limit?: number;
+
+  /**
+   * The index of the first album to return.
+   * 
+   * Default: `0` (i.e. the first album)
+   * 
+   * Use with limit to get the next set of albums
+   */
+  offset?: number;
+}
+
+/**
+ * Get Spotify catalog information about an artist’s albums
+ * 
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-artists-albums
+ */
+export type GetArtistAlbumsResponse = PagingObject<SimplifiedAlbumObject>;
