ac4b4141601dd5d65089b5a6daf9817a1e130263
Shubham Parihar <shubhamparihar391@gmail.com>
2021-05-07 09:33:13 +0530

    types: add typings for audio features

--- v1/rest/track.ts
+++ v1/rest/track.ts
@@ -1,4 +1,4 @@
-import type { TrackObject } from '../payloads/track';
+import type { AudioFeaturesObject, TrackObject } from '../payloads/track';
 
 /**
  * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-several-tracks
@@ -31,3 +31,25 @@ export type GetTrackQuery = Omit<GetMultipleTracksQuery, 'ids'>;
  * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-track
  */
 export type GetTrackResponse = TrackObject;
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-several-audio-features
+ */
+export interface GetMultipleTracksAudioFeaturesQuery {
+  /**
+   * A comma-separated list of the Spotify IDs for the tracks. Maximum: `100` IDs
+   */
+  ids: Array<string>;
+}
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-several-audio-features
+ */
+export interface GetMultipleTracksAudioFeaturesResponse {
+  audio_features: Array<AudioFeaturesObject | null>;
+}
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-audio-features
+ */
+export type GetTrackAudioFeaturesResponse = AudioFeaturesObject;
