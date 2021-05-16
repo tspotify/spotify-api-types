cf760a8f052a890cc25032baa9ee06f7f32752ad
Shubham Parihar <shubhamparihar391@gmail.com>
2021-05-16 17:34:40 +0530

    types(rest/misc): add typings for recommendations endpoint query

--- v1/rest/misc.ts
+++ v1/rest/misc.ts
@@ -1,3 +1,5 @@
+import type { Limit_O, Market_O } from "./util";
+
 /**
  * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-available-markets
  */
@@ -17,4 +19,108 @@ export interface GetRecommendationGenresResponse {
    * A list of available genres
    */
   genres: Array<string>;
+}
+
+/**
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-recommendations
+ */
+export interface GetRecommendationsResponse extends Limit_O, Market_O {
+  /**
+   * A comma separated list of Spotify IDs for seed artists. Up to 5 seed values may be provided in any combination of `seed_artists`, `seed_tracks` and `seed_genres`
+   */
+  seed_artists: string;
+
+  /**
+   * A comma separated list of any genres in the set of available genre seeds. Up to 5 seed values may be provided in any combination of `seed_artists`, `seed_tracks` and `seed_genres`
+   */
+  seed_genres: string
+
+  /**
+   * A comma separated list of Spotify IDs for a seed track. Up to 5 seed values may be provided in any combination of `seed_artists`, `seed_tracks` and `seed_genres`
+   */
+  seed_tracks: string;
+
+  min_acousticness?: number;
+
+  max_acousticness?: number;
+
+  target_acousticness?: number;
+
+  min_danceability?: number;
+
+  max_danceability?: number;
+
+  target_danceability?: number;
+
+  min_duration_ms?: number;
+
+  max_duration_ms?: number;
+
+  target_duration_ms?: number;
+
+  min_energy?: number;
+
+  max_energy?: number;
+
+  target_energy?: number;
+
+  min_instrumentalness?: number;
+
+  max_instrumentalness?: number;
+
+  target_instrumentalness?: number;
+
+  min_key?: number;
+
+  max_key?: number;
+
+  target_key?: number;
+
+  min_liveness?: number;
+
+  max_liveness?: number;
+
+  target_liveness?: number;
+
+  min_loudness?: number;
+
+  max_loudness?: number;
+
+  target_loudnes?: number;
+
+  min_mode?: number;
+
+  max_mode?: number;
+
+  target_mode?: number;
+
+  min_popularity?: number;
+
+  max_popularity?: number;
+
+  target_popularity?: number;
+
+  min_speechiness?: number;
+
+  max_speechiness?: number;
+
+  target_speechiness?: number;
+
+  min_tempo?: number;
+
+  max_tempo?: number;
+
+  target_tempo?: number;
+
+  min_time_signature?: number;
+
+  max_time_signature?: number;
+
+  target_time_signature?: number;
+
+  min_valence?: number;
+
+  max_valence?: number;
+
+  target_valence?: number;
 }
